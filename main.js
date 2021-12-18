831002439b61c7449529dc57769127e5ed7cba62
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2021-12-18 22:19:44 +0500

    Day 26
    Done with Day 26 of Javascript Coding

--- main.js
+++ main.js
@@ -915,3 +915,20 @@ function multiplyBy2() {
   }
 }
 multiplyBy2();
+//Use Caution When Reinitializing Variables Inside a Loop
+function zeroArray(m, n) {
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+       row.push(0);
+    }
+    newArray.push(row);
+    row = [];
+  }
+   row = [];
+  return newArray;
+
+}
+let matrix = zeroArray(3,2);
+console.log(matrix);
