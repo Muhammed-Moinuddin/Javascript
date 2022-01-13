6b096008b36bfd936de6a3648e5bb0760802d006
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-01-13 23:36:41 +0500

    Day 42
    Done with Day 42 of Javascript Coding

--- main.js
+++ main.js
@@ -1321,3 +1321,13 @@ function titleCase(str) {
   }
   return updatedTitle.join(" ");
 }
+//Title Case a Sentence
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(val => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+    .join(" ");
+}
+
+titleCase("I'm a little tea pot");
