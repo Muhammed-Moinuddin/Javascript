8d1de3335e1295993acff949dd565822fbaa520c
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-03-11 23:16:23 +0500

    Day 71
    Done with Day 71 of Javascript Coding

--- main.js
+++ main.js
@@ -2167,4 +2167,153 @@ function destroyer(arr) {
 }
 
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
-//Wherefore art thou
+//Was doing 5 projects work (After below solution everything is aligned).
+//Roman Numeral Converter
+//A bit Complicated Way
+function convertToRoman(num) {
+  const romanArray = []
+  const some = [
+    {
+    1 : "I",
+    2 : "II",
+    3 : "III",
+    4 : "IV",
+    5 : "V",
+    6 : "VI",
+    7 : "VII",
+    8 : "VIII",
+    9 : "IX",
+    0 : ""
+    },
+   {
+    1 : "X",
+    2 : "XX",
+    3 : "XXX",
+    4 : "XL",
+    5 : "L",
+    6 : "LX",
+    7 : "LXX",
+    8 : "LXXX",
+    9 : "XC",
+    0 : ""
+   },
+    {
+    1 : "C",
+    2 : "CC",
+    3 : "CCC",
+    4 : "CD",
+    5 : "D",
+    6 : "DC",
+    7 : "DCC",
+    8 : "DCCC",
+    9 : "CM",
+    0 : ""
+    },
+
+  ]
+ let thousandNum = "";
+ let numArray = num.toString().split("").reverse();
+ let checkArray = []
+
+  console.log(numArray);
+  if (numArray.length < 4) {
+    for (let i = 0 ; i < some.length ; i++){
+     for (let j = i ; j <= numArray.length ; j++){
+      checkArray.push(some[i][numArray[j]])
+      break
+     }
+    }
+  } else if (numArray.length > 3) {
+    for (let i = 3 ; i < numArray.length ; i++) {
+      for (let j = 0 ; j < numArray[i] ; j++) {
+        thousandNum = thousandNum +"M";
+      }
+    }
+
+   numArray.splice(3);
+   for (let i = 0 ; i < some.length ; i++){
+     for (let j = i ; j <= numArray.length ; j++){
+      checkArray.unshift(some[i][numArray[j]]);
+      break
+     }
+    }
+    checkArray.reverse()
+    checkArray.push(thousandNum);
+  }
+ checkArray.reverse();
+ let answer = checkArray.join("")
+ return answer;
+ }
+ console.log(convertToRoman(1023))
+//Straight Forward Way
+function convertToRoman(num) {
+  const romanArray = []
+  const some = [
+    {
+    1 : "I",
+    2 : "II",
+    3 : "III",
+    4 : "IV",
+    5 : "V",
+    6 : "VI",
+    7 : "VII",
+    8 : "VIII",
+    9 : "IX",
+    0 : ""
+    },
+   {
+    1 : "X",
+    2 : "XX",
+    3 : "XXX",
+    4 : "XL",
+    5 : "L",
+    6 : "LX",
+    7 : "LXX",
+    8 : "LXXX",
+    9 : "XC",
+    0 : ""
+   },
+    {
+    1 : "C",
+    2 : "CC",
+    3 : "CCC",
+    4 : "CD",
+    5 : "D",
+    6 : "DC",
+    7 : "DCC",
+    8 : "DCCC",
+    9 : "CM",
+    0 : ""
+    },
+    {
+    1 : "M",
+    2 : "MM",
+    3 : "MMM",
+    4 : "MMMM",
+    5 : "MMMMM",
+    6 : "MMMMMM",
+    7 : "MMMMMMM",
+    8 : "MMMMMMMM",
+    9 : "MMMMMMMMM",
+    0 : "M"
+    }
+
+  ]
+ let thousandNum = "";
+ let numArray = num.toString().split("").reverse();
+ let checkArray = []
+
+  console.log(numArray);
+  if (numArray.length < 6) {
+    for (let i = 0 ; i < some.length ; i++){
+     for (let j = i ; j <= numArray.length ; j++){
+      checkArray.push(some[i][numArray[j]])
+      break
+     }
+    }
+  }
+ checkArray.reverse();
+ let answer = checkArray.join("");
+ return answer;
+ }
+ console.log(convertToRoman(1000))
