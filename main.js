45bbe6e18e3ffb758ef4a23db81cff014c4700c8
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-03-16 23:55:19 +0500

    Day 72
    Done with Day 72 of Javascript Coding

--- main.js
+++ main.js
@@ -2317,3 +2317,45 @@ function convertToRoman(num) {
  return answer;
  }
  console.log(convertToRoman(1000))
+ //Caesars Cipher
+ function rot13(str) {
+  const  obj1 = {
+   "A" : "N",
+   "B" : "O",
+   "C" : "P",
+   "D" : "Q",
+   "E" : "R",
+   "F" : "S",
+   "G" : "T",
+   "H" : "U",
+   "I" : "V",
+   "J" : "W",
+   "K" : "X",
+   "L" : "Y",
+   "M" : "Z",
+   "N" : "A",
+   "O" : "B",
+   "P" : "C",
+   "Q" : "D",
+   "R" : "E",
+   "S" : "F",
+   "T" : "G",
+   "U" : "H",
+   "V" : "I",
+   "W" : "J",
+   "X" : "K",
+   "Y" : "L",
+   "Z" : "M"
+  };
+  let check = [];
+  for (let i = 0 ; i < str.length ; i++) {
+    if (obj1.hasOwnProperty(str[i]) == true) {
+      check.push(obj1[str[i]])
+  } else {
+    check.push(str[i])
+  }
+  }
+  return check.join("");
+}
+
+console.log(rot13("SERR CVMMN!"));
