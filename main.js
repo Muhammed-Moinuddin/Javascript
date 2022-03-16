cc9c45bbf9db775146ee2929e82d56ce091d3a45
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-03-17 00:56:29 +0500

    Day 73
    Done with Day 73 of Javascript Coding

--- main.js
+++ main.js
@@ -2359,3 +2359,10 @@ function convertToRoman(num) {
 }
 
 console.log(rot13("SERR CVMMN!"));
+//Wherefore art thou
+function whatIsInAName(collection, source) {
+  const sourceKey = Object.keys(source);
+  return collection.filter(obj => sourceKey.every(key => obj.hasOwnProperty(key)&& obj[key] == source[key]));
+ }
+
+ console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
