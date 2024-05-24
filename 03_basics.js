987fe9e3788ea9d3611076835f47d02372bcfee6
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2024-05-25 03:10:21 +0500

    Completed Till IIFE

--- 03_basics.js
+++ 03_basics.js
@@ -9,3 +9,37 @@ function myCart2(username = "moin"){
 }
 
 console.log(myCart2("ali"));
+
+const user = {
+    username: "moin",
+    price: 999,
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+    }
+}
+
+user.welcomeMessage();
+console.log(this);
+
+//In this example, this refers to the person object.
+const person = {
+    firstName  : "John",
+    lastName   : "Doe",
+    id     : 5566,
+    myFunction : function() {
+      return this;
+    }
+};
+console.log(person.myFunction().firstName);
+
+//IIFE
+
+//named IIFE
+(function chai(){
+    console.log("DB connected");
+})();
+
+((myName) => {
+    console.log(myName);
+})("Moinuddin here");
